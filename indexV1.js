9cb08d3b4ecbb138e240ab3cbef39cd9a7d39a5f
Edleron <Edleron@hotmail.com>
2023-01-13 23:46:18 +0300

    gpt random numbers v3

--- indexV1.js
+++ indexV1.js
@@ -1,9 +1,9 @@
-function getRandomNumbers() {
+function getRandomNumbers(num_count) {
     let randomNumbers = [];
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < num_count; i++) {
         let randomNumber = Math.floor(Math.random() * (60 - 1 + 1)) + 1;
         randomNumbers.push(randomNumber);
     }
     return randomNumbers;
 }
-console.log(getRandomNumbers());
+console.log(getRandomNumbers(6));
