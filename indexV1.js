cd40e75b3651d4b159f1bfccb3a0c0b463d44351
Edleron <Edleron@hotmail.com>
2023-01-14 12:26:34 +0300

    gpt random numbers v4

--- indexV1.js
+++ indexV1.js
@@ -1,9 +1,18 @@
-function getRandomNumbers(num_count) {
-    let randomNumbers = [];
-    for (let i = 0; i < num_count; i++) {
-        let randomNumber = Math.floor(Math.random() * (60 - 1 + 1)) + 1;
-        randomNumbers.push(randomNumber);
+var givenNumbers = [3, 11, 18, 34, 35, 41];
+var newSets = [];
+
+for (var i = 0; i < givenNumbers.length; i++) {
+    for (var j = i + 1; j < givenNumbers.length; j++) {
+        var selectedNumber1 = givenNumbers[i];
+        var selectedNumber2 = givenNumbers[j];
+        var newSet = [];
+
+        for (var k = 1; k <= 60; k++) {
+            if (!givenNumbers.includes(k) || k === selectedNumber1 || k === selectedNumber2) {
+                newSet.push(k);
+            }
+        }
+        newSets.push([i, j, newSet]);
+        console.log(`Set : ${newSet[i, j, newSet]}`);
     }
-    return randomNumbers;
 }
-console.log(getRandomNumbers(6));
